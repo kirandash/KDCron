5d6ba06c9ce01b4959307bcf5f9acc049d6b599a
kirandash <thekirankumardash@gmail.com>
2016-07-26 21:10:34 +0530

    2-Custom-Intervals

--- index.php
+++ index.php
@@ -11,12 +11,21 @@ Plugin URI: http://bgwebagency.com
 
 add_action('init', function(){//wordpress is fully loaded but has not sent any header yet
 	if(!wp_next_scheduled('kd_cron_hook')){//if there is no other cron job scheduled
-		wp_schedule_event(time(), 'hourly', 'kd_cron_hook', (array)$args)	;
+		wp_schedule_event(time(), 'two-minutes', 'kd_cron_hook', (array)$args)	;
 	}
 });
 
 add_action('kd_cron_hook', function(){
 	$str = time();
 	wp_mail('kiran@xhtmlchamps.com', 'Scheduled with WP_Cron!', "This email was sent at $str.");
+});
+
+// Custom cron scheduling
+add_filter('cron_schedules', function($schedules){
+	$schedules['two-minutes'] = array(
+		'interval'=>120,
+		'display'=>'Every Two Minutes'
+	);
+	return $schedules;
 });
 ?>
